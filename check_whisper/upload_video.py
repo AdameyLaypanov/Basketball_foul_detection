f3bad60cc543610bc014753bd1a1560d6a99ee8b
AdameyLaypanov <adlaynov@rambler.ru>
2025-03-22 15:01:32 +0300

    delete

--- check_whisper/upload_video.py
+++
@@ -1,9 +0,0 @@
-from pytubefix import YouTube
-
-def download_video(url):
-    # Очистка URL от лишних параметров
-    url = url.split('?')[0]  # Убираем все параметры после "?"
-    yt = YouTube(url)  # Используем pytubefix для загрузки видео
-    audio_stream = yt.streams.filter(only_audio=True).first()  # Получаем поток с аудио
-    audio_stream.download(output_path="audio", filename="video_audio.mp4")  # Скачиваем аудио
-    return "audio/video_audio.mp4"
